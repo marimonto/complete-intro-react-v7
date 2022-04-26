698f3ce72415c37d2fd59377ac30689e036492d4
mariana montoya <marimontod@gmail.com>
2022-04-26 07:39:29 -0500

    02-js-tools

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.brand),
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./pet";
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", { id: "brand" }, "Adopt Me!"),
-    React.createElement(Pet,{
-        name: "Trixie",
-        animal: "Cat",
-        brand: "Creole"
+    React.createElement(Pet, {
+      name: "Trixie",
+      animal: "Cat",
+      brand: "Creole",
     }),
-    React.createElement(Pet,{
-        name: "Fruna",
-        animal: "Cat",
-        brand: "Creole"
+    React.createElement(Pet, {
+      name: "Fruna",
+      animal: "Cat",
+      brand: "Creole",
     }),
-    React.createElement(Pet,{
-        name: "Matias",
-        animal: "Dog",
-        brand: ""
+    React.createElement(Pet, {
+      name: "Matias",
+      animal: "Dog",
+      brand: "Creole",
     }),
-    React.createElement(Pet,{
-        name: "Brandy",
-        animal: "Dog",
-        brand: ""
+    React.createElement(Pet, {
+      name: "Brandy",
+      animal: "Dog",
+      brand: "",
     }),
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
