4fa83b3f37f72092e5cb7c14bca78b5e79277848
mariana montoya <marimontod@gmail.com>
2022-04-25 20:43:03 -0500

    01-no-frills-react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.brand),
+  ]);
+};
+
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt Me!")
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", { id: "brand" }, "Adopt Me!"),
+    React.createElement(Pet,{
+        name: "Trixie",
+        animal: "Cat",
+        brand: "Creole"
+    }),
+    React.createElement(Pet,{
+        name: "Fruna",
+        animal: "Cat",
+        brand: "Creole"
+    }),
+    React.createElement(Pet,{
+        name: "Matias",
+        animal: "Dog",
+        brand: ""
+    }),
+    React.createElement(Pet,{
+        name: "Brandy",
+        animal: "Dog",
+        brand: ""
+    }),
+  ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
