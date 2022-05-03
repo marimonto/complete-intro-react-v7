6e41c9b9b109b86aca702d09258263eb0d26cb39
mariana montoya <marimontod@gmail.com>
2022-05-02 19:14:27 -0500

    03-jsx

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
-import React from "react";
 import { render } from "react-dom";
-import Pet from "./pet";
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "brand" }, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Trixie",
-      animal: "Cat",
-      brand: "Creole",
-    }),
-    React.createElement(Pet, {
-      name: "Fruna",
-      animal: "Cat",
-      brand: "Creole",
-    }),
-    React.createElement(Pet, {
-      name: "Matias",
-      animal: "Dog",
-      brand: "Creole",
-    }),
-    React.createElement(Pet, {
-      name: "Brandy",
-      animal: "Dog",
-      brand: "",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Trixie" animal="Cat" brand="Creole" />
+      <Pet name="Fruna" animal="Cat" brand="Creole" />
+      <Pet name="Matias" animal="Dog" brand="Creole" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
